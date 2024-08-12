fb41d6bea1eda24f1c2a0082c3810584c215d2f3
Tobias Maciel <tobiasmaciel03@gmail.com>
2024-08-12 19:01:44 -0300

    feat: Se conecta el eslint con el prettier

--- FrontAdmin/.eslintrc.cjs
+++ FrontAdmin/.eslintrc.cjs
@@ -5,11 +5,13 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "prettier"],
   rules: {
+    "prettier/prettier": "warn", // Se muestra los errores de Prettier como warnings
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
