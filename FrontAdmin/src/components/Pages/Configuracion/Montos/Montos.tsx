42b4821d6417e917bb789254b0a482319b3491a5
Facundo Melgarejo Roma <facundomelgarejor@gmail.com>
2024-08-20 20:15:51 -0300

    feat: modificaciones page montos

--- FrontAdmin/src/components/Pages/Configuracion/Montos/Montos.tsx
+++ FrontAdmin/src/components/Pages/Configuracion/Montos/Montos.tsx
@@ -7,12 +7,14 @@ import {
   SimpleGrid,
   useDisclosure,
   IconButton,
+  Tooltip
 } from '@chakra-ui/react';
 import { EditIcon } from '@chakra-ui/icons';
 import ModalComponent from '../../../Modal/ModalConfirmarCambios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useToast } from '@chakra-ui/react';
 import  VerHistorial  from './ModalVerHistorial';
+import { FetchMontos } from '../../../../API/Montos';
 
 const MontoInput = ({ label, name, value, onChange, isReadOnly }: { label: string, name: string, value: number, onChange: (e: { target: { name: string; value: string } }) => void, isReadOnly: boolean }) => (
   <Flex align="center">
@@ -25,15 +27,57 @@ const MontoInput = ({ label, name, value, onChange, isReadOnly }: { label: strin
       size="sm"
       bg="white"
       readOnly={isReadOnly}
-      _readOnly={{ opacity: 1, cursor: 'default' }}
+      _readOnly={{ opacity: 1, cursor: 'not-allowed', bg:"gray.50", color:"gray.700" }}
     />
   </Flex>
 );
 
-//utilizar fecha ultima vez que se cargo el archivo/montos
-const fechaUltimoPago = '2024 - 2C';
+
+
+
 
 const Montos = () => {
+  //utilizar fecha ultima vez que se cargo el archivo/montos
+interface Archivo {
+  id_comp_pago: number;
+  archivo_pdf_url: string;
+  cuatrimestre: string;
+  monto_completo: number;
+  monto_completo_2venc: number;
+  monto_completo_3venc: number;
+  matricula: number;
+  cuota_reducida: number;
+  cuota_reducida_2venc: number;
+  cuota_reducida_3venc: number;
+  fecha_carga_comp_pdf: string;
+}
+
+const [fechaUltimoPago, setFechaUltimoPago] = useState('');
+
+useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await FetchMontos();
+
+        if (data && data.length > 0) {
+          // Encontrar el archivo con la última fecha de carga
+          const ultimoArchivo = data.reduce((prev: Archivo, current: Archivo) => {
+            const prevDate = new Date(prev.fecha_carga_comp_pdf);
+            const currentDate = new Date(current.fecha_carga_comp_pdf);
+            return prevDate > currentDate ? prev : current;
+          });
+
+          // Guardar la fecha del último archivo en el estado
+          const fechaFormateada = new Date(ultimoArchivo.fecha_carga_comp_pdf).toLocaleDateString();
+          setFechaUltimoPago(fechaFormateada);
+        }
+      } catch (error) {
+        console.error("Error al obtener los montos:", error);
+      }
+    };
+    fetchData();
+})
+
   const { isOpen: isOpenModal1, onOpen: onOpenModal1, onClose: onCloseModal1 } = useDisclosure();
   const { isOpen: isOpenModal2, onOpen: onOpenModal2, onClose: onCloseModal2 } = useDisclosure();
   const confirmarMontos = useToast();
@@ -84,21 +128,37 @@ const Montos = () => {
       mt={4}
       position="relative"
     >
-      <IconButton
-        icon={<EditIcon />}
-        aria-label="Editar"
-        size="sm"
-        position="absolute"
-        top={2}
-        right={2}
-        onClick={() => setIsEditing(!isEditing)}
-        color={isEditing ? 'green.500' : 'gray.500'}
-        bgColor={isEditing ? 'green.100' : 'gray.100'}
-        _hover={{ color: 'green.600', bgColor: 'green.200' }}
-      />
-      <Text fontSize="2xl" fontWeight="bold" mb={4}>
-        Montos
-      </Text>
+        <Tooltip
+        label="Editar"
+        placement="bottom"
+        p="10px"
+        bg="white"
+        color="black"
+        hasArrow
+        borderRadius="10px"
+      >
+
+        <IconButton
+          icon={<EditIcon color="black"/>}
+          aria-label="Editar"
+          size="lg"
+          position="absolute"
+          top={2}
+          right={2}
+          onClick={() => setIsEditing(!isEditing)}
+          color={isEditing ? 'green.500' : 'gray.500'}
+          bgColor={isEditing ? 'green.100' : 'white'}
+          _hover={{ color: 'green.600', bgColor: 'green.200' }}
+        />
+        </Tooltip>
+        <SimpleGrid columns={8} spacing={0}>
+          <Text fontSize="2xl" fontWeight="bold" mb={4}>
+            Montos
+          </Text>
+          <Text fontWeight="bold"  mb={4} mt={2}>
+            Periodo {fechaUltimoPago}
+          </Text>
+        </SimpleGrid>
       <SimpleGrid columns={2} spacing={2}>
         <MontoInput
           isReadOnly={!isEditing}
@@ -152,10 +212,10 @@ const Montos = () => {
       </SimpleGrid>
 
       <Flex justify="flex-end" gap="4" pt={{ base: '30px', md: '0' }}>
-        <Button color="white"  onClick={onOpenModal1} isDisabled={!isEditing}>
+        <Button color="white" size="sm"  onClick={onOpenModal1} isDisabled={!isEditing} _hover={isEditing ? 'none' : 'auto'}>
           Guardar Cambios
         </Button>
-        <Button color="white" onClick={onOpenModal2}>
+        <Button color="white"  size="sm"  onClick={onOpenModal2}>
           Ver Historial
         </Button>
       </Flex>
