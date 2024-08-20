0d53101f389d3c6ca967095ae6f436f64bbcba64
Tobias Maciel <tobiasmaciel03@gmail.com>
2024-08-20 01:49:15 -0300

    feat: Se agrega que dependiendo del cuatrimestre se muestre el otro

--- FrontAdmin/src/components/Pages/Configuracion/ModalCargarDocumento.tsx
+++ FrontAdmin/src/components/Pages/Configuracion/ModalCargarDocumento.tsx
@@ -14,13 +14,24 @@ import {
   Image,
   Text,
   Heading,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
 } from '@chakra-ui/react';
 import compPago from '../../icons/compromiso_de_pago_2023.pdf';
 import Dropzone from './DropZone';
+import { useEffect, useState } from 'react';
+
+interface Compromiso {
+  fecha_carga_comp_pdf: string;
+  cuatrimestre: string;
+}
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   titleModal: string;
+  compromiso: Compromiso;
 }
 
 //funcion que dependiendo lo que devuelva el endpoint /compromisos/ verifique la fecha de carga del ultimo
@@ -32,7 +43,43 @@ const ModalCargarDocumento: React.FC<ModalProps> = ({
   isOpen,
   onClose,
   titleModal,
+  compromiso,
 }) => {
+  const añoActual = new Date().getFullYear();
+  const añoCompromiso = new Date(
+    compromiso?.fecha_carga_comp_pdf
+  ).getUTCFullYear();
+  const [cuatrimestre, setCuatrimestre] = useState('');
+  const [aviso, setAviso] = useState(false);
+
+  useEffect(() => {
+    if (añoActual == añoCompromiso) {
+      if (compromiso?.cuatrimestre === '1C') {
+        setCuatrimestre('2C');
+      } else {
+        setCuatrimestre('1C');
+      }
+    }
+  }, []);
+
+  const handleSelect = () => {
+    if (cuatrimestre == '1C') {
+      setCuatrimestre('2C');
+    } else {
+      setCuatrimestre('1C');
+    }
+  };
+
+  useEffect(() => {
+    if (añoActual == añoCompromiso) {
+      if (compromiso?.cuatrimestre === cuatrimestre) {
+        setAviso(true);
+      } else {
+        setAviso(false);
+      }
+    }
+  }, [cuatrimestre]);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -41,8 +88,16 @@ const ModalCargarDocumento: React.FC<ModalProps> = ({
         <ModalCloseButton />
         <ModalBody mb="30px">
           <Text mb="20px">¿A que cuatrimestre corresponde el archivo?</Text>
+          {aviso && (
+            <Alert status="warning" mb="20px">
+              <AlertIcon />
+              Ya existe un compromiso de pago para ese cuatrimestre.
+            </Alert>
+          )}
           <Select
             placeholder="Selecciona un cuatrimestre"
+            value={cuatrimestre}
+            onChange={handleSelect}
             name="cuatrimestre"
             mb="20px"
           >
