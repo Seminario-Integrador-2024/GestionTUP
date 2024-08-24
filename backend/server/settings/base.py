ead00e5d8397e69abbb7b60ad391172a96bd9912
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2024-08-24 08:41:03 -0300

    chore: Add allauth account middleware for authentication

--- backend/server/settings/base.py
+++ backend/server/settings/base.py
@@ -93,6 +93,7 @@ MIDDLEWARE: list[str] = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "server.urls"
