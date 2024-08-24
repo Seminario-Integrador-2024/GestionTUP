c59f3cc3327a98146e3e699525cf3fed1d00e182
Facundo Melgarejo Roma <facundomelgarejor@gmail.com>
2024-08-24 13:49:07 -0300

    feat: Creacion submenu, pagina alumnos que cursan una materia

---
+++ FrontAdmin/src/components/SubMenu/LinksSubMenu.tsx
@@ -0,0 +1,10 @@
+export const ITEMS_SUBMENU = [
+    { url:'#', title: 'Alumnos inhabilitados' },
+    { url:'#', title: 'Alumnos con baja provisoria'},
+    { url:'#', title: 'Alumnos activos' },
+    { url:'#', title: 'Alumnos que abonaron cuota'},
+    { url:'#', title: 'Alumnos que no abonaron cuota' },
+    { url:'#', title: 'Alumnos que no abonaron matricula'},
+    { url:'#', title: 'Pagos' },
+    { url:'alumnos-que-cursan-materia', title: 'Alumnos que cursan una materia'},
+  ];
