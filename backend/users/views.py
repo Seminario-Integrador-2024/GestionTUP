6cbdc5b52c78fa8750dd987e039e2a4fc5ce65be
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2024-08-27 17:04:15 -0300

    feat: Add @react-oauth/google package for Google OAuth2 login

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+
+# API Login View with Google OAuth2
+
+
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+from django.shortcuts import redirect
+from rest_framework.views import APIView
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
+    callback_url = "http://localhost:8000/accounts/google/login/callback/"
