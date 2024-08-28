d36b57b053e6179d7d3d86eecd3da6721774d8f8
Tobias Maciel <tobiasmaciel03@gmail.com>
2024-08-27 23:53:04 -0300

    feat: Fetch materias

--- FrontAdmin/src/components/Pages/Estadisticas/SubPages/ListadoMaterias.tsx
+++ FrontAdmin/src/components/Pages/Estadisticas/SubPages/ListadoMaterias.tsx
@@ -1,138 +1,119 @@
 import React, { useState, ChangeEvent, useEffect } from 'react';
-import { useNavigate, Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
-    Container,
-    Box,
-    Text,
-    Heading,
-    List,
-    ListItem,
-    VStack,
+  Container,
+  Box,
+  Text,
+  Heading,
+  List,
+  ListItem,
+  VStack,
+  Button,
 } from '@chakra-ui/react';
 import CustomSelect from './Seleccion';
 import { LINK_MATERIAS } from '../../../Subjects/LinksMaterias';
 import { FetchMaterias } from '../../../../API/Materias';
 
-type Cuatrimestre = 'primer-cuatrimestre' | 'segundo-cuatrimestre';
-
 const opcionesCuatrimestre = [
-    { value: 'primer-cuatrimestre', label: 'Primer Cuatrimestre' },
-    { value: 'segundo-cuatrimestre', label: 'Segundo Cuatrimestre' },
+  { value: '1', label: 'Primer Cuatrimestre' },
+  { value: '2', label: 'Segundo Cuatrimestre' },
 ];
 
-const [asignaturas, setAsignaturas] = useState()
-const [error, setError] = useState("")
-const [loading, setLoading] = useState(true)
-
-useEffect(() => {
-    const getMaterias = async () => {
-        try {
-            const data = await FetchMaterias();
-            setAsignaturas(data.results);
-        } catch (error) {
-            if (error instanceof Error) {
-                setError(error.message);
-            } else {
-                setError('An unknown error occurred');
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    getMaterias();
-}, []
-)
-
-const materias: Record<Cuatrimestre, string[]> = {
-    'primer-cuatrimestre': [
-        'Programación I',
-        'Arquitectura y Sistemas Operativos',
-        'Matemática',
-        'Organización Empresarial',
-        'Programación III',
-        'Base de Datos II',
-        'Metodología de Sistemas II',
-        'Ingles II',
-    ],
-    'segundo-cuatrimestre': [
-        'Programación II',
-        'Probabilidad y Estadistica',
-        'Base de Datos',
-        'Ingles I',
-        'Programación IV',
-        'Metodología de Sistemas II',
-        'Introducción al Análisis de Datos',
-        'Legislación',
-        'Gestión de Desarrollo de Software',
-        'Trabajo Final Integrador',
-    ],
-};
-
-interface MateriaLink {
-    title: string;
-    url: string;
+interface Materia {
+  nombre: string;
+  cuatrimestre: number; // Cambiado a number
 }
 
 const ListadoMaterias: React.FC = () => {
-    const [cuatrimestre, setSemester] = useState<Cuatrimestre | ''>('');
-    const navigate = useNavigate();
+  const [cuatrimestre, setCuatrimestre] = useState<number>(1); // Cambiado a number
+  const [asignaturas, setAsignaturas] = useState<Materia[]>([]);
+  const [filteredAsignaturas, setFilteredAsignaturas] = useState<Materia[]>([]);
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
 
-    const handleSemesterChange = (event: ChangeEvent<HTMLSelectElement>) => {
-        setSemester(event.target.value as Cuatrimestre);
-    };
+  const navigate = useNavigate();
 
-    const handleMateriaClick = (materia: string) => {
-        const materiaLink = LINK_MATERIAS.find(
-            (item: MateriaLink) => item.title === materia
-        );
-
-        if (materiaLink) {
-            const url = `${materiaLink.url}`;
-            navigate(url);
+  useEffect(() => {
+    const getMaterias = async () => {
+      try {
+        const data = await FetchMaterias();
+        setAsignaturas(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
         } else {
-            console.error('URL de materia no encontrada para:', materia);
+          setError('An unknown error occurred');
         }
+      } finally {
+        setLoading(false);
+      }
     };
 
-    const filteredSubjects = cuatrimestre ? materias[cuatrimestre] : [];
+    getMaterias();
+  }, []);
 
-    return (
-        <Container maxW="container.md" p={4}>
-            <VStack spacing={6} align="start">
-                <Heading as="h1" size="lg">
-                    Listado de Materias
-                </Heading>
-                <Box w="full">
-                    <CustomSelect
-                        placeholder="Seleccionar Cuatrimestre"
-                        options={opcionesCuatrimestre}
-                        value={cuatrimestre}
-                        onChange={handleSemesterChange}
-                    />
-                </Box>
-                {cuatrimestre && (
-                    <Box w="full">
-                        <List spacing={3}>
-                            {filteredSubjects.map((materia) => (
-                                <ListItem
-                                    key={materia}
-                                    p={2}
-                                    borderRadius="md"
-                                    _hover={{ bg: 'gray.100', cursor: 'pointer' }}
-                                    onClick={() => handleMateriaClick(materia)}
-                                >
-                                    <Text fontSize="md" color="gray.700">
-                                        {materia}
-                                    </Text>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Box>
-                )}
-            </VStack>
-        </Container>
+  useEffect(() => {
+    const filteredData = asignaturas.filter(
+      (asignatura) => asignatura.cuatrimestre === cuatrimestre
     );
+    setFilteredAsignaturas(filteredData);
+  }, [cuatrimestre, asignaturas]);
+
+  const handleSemesterChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCuatrimestre(parseInt(event.target.value, 10)); // Convertimos el valor a número
+  };
+
+  const handleMateriaClick = (materia: string) => {
+    if (materia) {
+      // Reemplazar espacios por guiones en el nombre de la materia
+      const formattedMateria = materia.replace(/\s+/g, '-');
+      const url = `${formattedMateria}`;
+      navigate(url);
+    } else {
+      console.error('Nombre de materia no proporcionado');
+    }
+  };
+
+  return (
+    <Container maxW="container.md" p={4}>
+      <VStack spacing={6} align="start">
+        <Heading as="h1" size="lg">
+          Listado de Materias
+        </Heading>
+        <Box w="full">
+          <CustomSelect
+            placeholder="Seleccionar Cuatrimestre"
+            options={opcionesCuatrimestre}
+            value={cuatrimestre.toString()} // Convertimos el número a string para CustomSelect
+            onChange={handleSemesterChange}
+          />
+        </Box>
+        {loading ? (
+          <Text>Cargando asignaturas...</Text>
+        ) : error ? (
+          <Text>Error: {error}</Text>
+        ) : (
+          <Box w="full">
+            <List spacing={3}>
+              {filteredAsignaturas.map((materia) => (
+                <ListItem
+                  key={materia.nombre}
+                  p={2}
+                  borderRadius="md"
+                  _hover={{ bg: 'gray.100', cursor: 'pointer' }}
+                  onClick={() => handleMateriaClick(materia.nombre)}
+                >
+                  <Text fontSize="md" color="gray.700">
+                    {materia.nombre}
+                  </Text>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        )}
+      </VStack>
+    </Container>
+  );
 };
 
 export default ListadoMaterias;
