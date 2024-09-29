11d261bf6b3c7c295d92ee4e6cbb92dd822d3344
yoelmarain <jmarain62@gmail.com>
2024-09-29 13:13:41 -0300

    Refactor submenu links and remove unnecessary item

--- FrontAdmin/src/components/SubMenu/LinksSubMenu.tsx
+++ FrontAdmin/src/components/SubMenu/LinksSubMenu.tsx
@@ -2,7 +2,6 @@ export const ITEMS_SUBMENU = [
   { url: '#', title: 'Alumnos inhabilitados' },
   { url: '#', title: 'Alumnos con baja provisoria' },
   { url: 'cuotas', title: 'Cuotas' , tooltip: 'Alumnos que abonaron/no abonaron una respectiva cuota' },
-  { url: '#', title: 'Alumnos que abonaron cuota actual' },
   { url: '#', title: 'Alumnos que no abonaron cuota' },
   { url: '#', title: 'Alumnos que no abonaron matricula' },
   { url: '#', title: 'Pagos' },
