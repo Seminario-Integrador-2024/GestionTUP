2e674416d3a1ccada5bb225642042683d1c451f6
Miranda Ariano <arianogato-@hotmail.com>
2024-10-30 15:13:09 -0300

    feat: conecte alumnos que abonaron matricula

---
+++ FrontAdmin/src/API/AlumnosAbonaronMatricula.ts
@@ -0,0 +1,34 @@
+import Cookies from 'js-cookie';
+
+const fetchAlumnosAbonaronMatricula = async (url: string): Promise<any> => {
+  try {
+    const token = Cookies.get('tokennn');
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error('Error en la respuesta del servidor');
+    }
+  } catch (error) {
+    throw new Error('Network error: ' + error);
+  }
+};
+
+export const FetchAbonaronMatricula = async (): Promise<any> => {
+  const url = 'http://localhost:8000/api/pagos/matricula/alumnos/';
+  return fetchAlumnosAbonaronMatricula(url);
+};
+
+export const FetchNoAbonaronMatricula = async (): Promise<any> => {
+  const url = 'http://localhost:8000/api/firmantes/alumnos-no-firmaron-ultimo-compromiso/';
+  return fetchAlumnosAbonaronMatricula(url);
+};
