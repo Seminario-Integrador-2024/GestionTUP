acbfc1766805a1fe3c20b137fb6a10cfc173cd60
Facundo Melgarejo <facundomelgarejor@gmail.com>
2024-11-17 21:37:02 -0300

    fix: :boom: Detalle de pagos, calculo de valor original, mora y cuota con mora

--- FrontAdmin/src/components/Pages-Alumnos/EstadoCuenta/EstadoCuenta.tsx
+++ FrontAdmin/src/components/Pages-Alumnos/EstadoCuenta/EstadoCuenta.tsx
@@ -24,6 +24,7 @@ interface Cuota {
     tipo: string;
     valorinformado: number;
     monto: number;
+    fecha_vencimiento: string,
     cuota_completa: boolean,
 }
 
@@ -60,6 +61,9 @@ interface CompromisoResponse {
   cuota_reducida_2venc: number,
   cuota_reducida_3venc: number,
   matricula: number,
+  fecha_vencimiento_2: number,
+  fecha_vencimiento_1: number,
+  fecha_vencimiento_3: number,
 }
 
 interface Pago {
@@ -142,7 +146,6 @@ function InformarPago() {
     const fetchCompromiso  = async () => {
       try {
         const data = await FetchCompromisos();
-        console.log(' compromiso:', data)
         setCompromiso(data)
       } catch (error) {
         setError(error);
@@ -182,7 +185,6 @@ function InformarPago() {
         const fetchDetalleCompromiso = async () => {
           try {
             const detalleData = await FetchDetalleCompromiso(compromisoFirmado.results[0].id_compromiso_de_pago);
-            console.log('detalle compromiso:', detalleData)
             setDetalleCompromiso(detalleData);
           } catch (error) {
             console.error('Error al obtener el detalle del compromiso', error);
@@ -219,33 +221,48 @@ function InformarPago() {
     ultimo_cursado: '-'
   };
 
-  const verificarMora = (fecha: string) => {
-    const [year, month, day] = fecha.split('-');
-    const dia = parseInt(day, 10);
+  const verificarMora = (fechaDeInforme: string, cuota_fechaVencimiento: string) => {
+    if (!cuota_fechaVencimiento) {
+      console.error('Fecha de vencimiento inválida:', cuota_fechaVencimiento);
+      return false; // Devuelve un valor predeterminado
+    }
+    const [year, month, day] = fechaDeInforme.split('-');
+    let DatefechaDeInforme = new Date(year + '-' + month + '-' + day);
 
-    if (dia > 15) {
+    //armar la fecha de vencimiento mes a mes 
+    const [year_vto, month_vto, day_vto] = cuota_fechaVencimiento.split('-');
+    let fechaCompleta_vto_1 = new Date( year_vto + '-' + month_vto + '-' + detalleCompromiso?.fecha_vencimiento_1)
+    let fechaCompleta_vto_2 = new Date( year_vto + '-' + month_vto + '-' + detalleCompromiso?.fecha_vencimiento_2);
+
+    if (DatefechaDeInforme > fechaCompleta_vto_2) {
       return  2
-    } else if (dia > 10) {
+    } else if (DatefechaDeInforme > fechaCompleta_vto_1) {
       return  1
     } else {
       return  0
     }
   };
 
-  const mostrarMontoConMora = (fecha: string, cuota_completa: boolean) => {
-    const mora = verificarMora(fecha); //poner fecha en formato 'aaaa-mm-dd' para simular la mora 
-  
+  const mostrarMontoConMora = (
+    fechaDeInforme: string,
+    cuota_completa: boolean,
+    cuota_fechaVencimiento: string
+  ) => {
+    if (!detalleCompromiso) return 0;
+    const mora = verificarMora(fechaDeInforme, cuota_fechaVencimiento);
+    
     switch (mora) {
       case 0:
-        return cuota_completa ? detalleCompromiso?.monto_completo : detalleCompromiso?.cuota_reducida;
+        return cuota_completa ? detalleCompromiso.monto_completo : detalleCompromiso.cuota_reducida;
       case 1:
-        return cuota_completa ? detalleCompromiso?.monto_completo_2venc : detalleCompromiso?.cuota_reducida_2venc; 
+        return cuota_completa ? detalleCompromiso.monto_completo_2venc : detalleCompromiso.cuota_reducida_2venc;
       case 2:
-        return cuota_completa ? detalleCompromiso?.monto_completo_3venc : detalleCompromiso?.cuota_reducida_3venc; 
+        return cuota_completa ? detalleCompromiso.monto_completo_3venc : detalleCompromiso.cuota_reducida_3venc;
       default:
-        return 0; // Manejo de error o default
+        return 0;
     }
   };
+  
 
   
     return (
@@ -368,11 +385,11 @@ function InformarPago() {
                           <Tr key={index}>
                             {pago.cuotas.map  (cuota => (
                               cuota.id_cuota === detail ? ( // Verifica cada cuota para mostrar solo las que coinciden
-                                <>
+                                <> 
                                   <Td textAlign="center">{cuota.nro_cuota}</Td>
                                   <Td textAlign="center">{'$ ' + new Intl.NumberFormat('es-ES').format(cuota.tipo === "Matrícula" ? (detalleCompromiso?.matricula ?? 0) : (cuotaCompleta ? (detalleCompromiso?.monto_completo) ?? 0 : (detalleCompromiso?.cuota_reducida) ?? 0) )}</Td>
                                   <Td textAlign="center">
-                                  {'$ ' + new Intl.NumberFormat('es-ES').format((mostrarMontoConMora(pago.fecha, cuota.cuota_completa) ?? 0) - (cuota.cuota_completa ? (detalleCompromiso?.monto_completo) ?? 0 : (detalleCompromiso?.cuota_reducida) ?? 0) ) }
+                                  {'$ ' + new Intl.NumberFormat('es-ES').format(cuota.tipo === "Matrícula" ? 0 : (mostrarMontoConMora(pago.fecha, cuota.cuota_completa, cuota.fecha_vencimiento) ?? 0) - (cuota.cuota_completa ? (detalleCompromiso?.monto_completo) ?? 0 : (detalleCompromiso?.cuota_reducida) ?? 0) ) }
                                     </Td>
                                   <Td textAlign="center">{'$ ' + new Intl.NumberFormat('es-ES').format(cuota.monto)}</Td>
                                   <Td textAlign="center">{formatoFechaISOaDDMMAAAA(pago.fecha)}</Td>
@@ -395,52 +412,3 @@ function InformarPago() {
 }
 
 export default InformarPago;
-
-/*
-           <Tabs ml="30px">
-              <TabList>
-                <Tab>Estado de cuenta</Tab>
-              </TabList>
-
-              <TabPanels>
-                <TabPanel minW="50vw">
-                      <Tag m="20px" p="10px">
-                    Estado de cuenta al {fechaDeHoy} 
-                  </Tag>
-                  {pagos.length > 1 ? (
-                    <Table variant="simple" width="100%">
-                      <Thead>
-                        <Tr mt={6}>
-                          <Th textAlign="center" p={1}>
-                            Numero
-                          </Th>
-                          <Th textAlign="center">Fecha Primer Vto.</Th>
-                          <Th textAlign="center">Valor Actual</Th>
-                          <Th textAlign="center">Valor Pagado</Th>
-                          <Th textAlign="center">Valor Informado</Th>
-                          <Th textAlign="center">Valor Adeudado</Th>
-                        </Tr>
-                      </Thead>
-                      <Tbody>
-                        {pagos && pagos.map((pago, index) => (
-                          <Tr key={index}>
-                            <Td textAlign="center" p={1}>
-                              {pago.numero}
-                            </Td>
-                            <Td textAlign="center">{}</Td>
-                            <Td textAlign="center">{'$ ' + new Intl.NumberFormat('es-ES').format(pago.montoActual)}</Td>
-                            <Td textAlign="center">{'$ ' + new Intl.NumberFormat('es-ES').format(pago.valorpagado)}</Td>
-                            <Td textAlign="center">{'$ ' + new Intl.NumberFormat('es-ES').format(pago.valorinformado)}</Td>
-                            <Td textAlign="center">{'$ ' + new Intl.NumberFormat('es-ES').format(pago.montoActual - pago.valorpagado - pago.valorinformado)}</Td>
-                          </Tr>
-                        ))}
-                      </Tbody>
-                    </Table>
-                  ) : (
-                    <Text  textAlign="center" padding="20px">No existen cuotas del cuatrimestre en curso. El alumno no firmo el compromiso de pago del periodo actual.</Text>
-                  )}
-                </TabPanel>
-              
-              </TabPanels>
-      </Tabs>
-*/
