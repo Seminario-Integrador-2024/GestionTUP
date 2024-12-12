729f5c72606144bd1b5985f5595ffd9aa814e169
Facundo Melgarejo <facundomelgarejor@gmail.com>
2024-12-12 17:28:58 -0300

    fix matricula

--- FrontAdmin/src/components/SubMenu/LinksSubMenu.tsx
+++ FrontAdmin/src/components/SubMenu/LinksSubMenu.tsx
@@ -4,7 +4,7 @@ export const ITEMS_SUBMENU = [
   { url: 'inhabilitaciones', title: 'Habilitar/Inhabilitar', tooltip: 'Alumnos que se encuentran inhabilitados' },
   { url: 'baja-provisoria', title: 'Baja Provisoria', tooltip: 'Alumnos que solicitaron la baja' },
   { url: 'cuotas', title: 'Cuotas' , tooltip: 'Alumnos que abonaron/adeudan una respectiva cuota' },
-  { url: 'matricula', title: 'Matricula', tooltip: 'Alumnos que abonaron la matricula' },
+  { url: 'matricula', title: 'Matrícula', tooltip: 'Alumnos que abonaron la matrícula' },
   { url: 'alumnos-que-firmaron-compromiso-de-pago', title: 'Compromiso de Pago', tooltip: 'Alumnos que firmaron el compromiso de pago' },
   { url: 'pagos', title: 'Pagos', tooltip: 'Pagos confirmados en un periodo de tiempo' },
   { url: 'deudas', title: 'Deudas', tooltip: 'Alumnos que adeudan una o mas cuotas' },
